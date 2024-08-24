885d7e0f9160d10a892d5c78eb81953b4b57cf3c
MirayAtass <mirayatasss@gmail.com>
2024-08-24 21:50:33 +0300

    borrowed and returned transaction

--- index.js
+++ index.js
@@ -20,11 +20,26 @@ app.get('/users', (req, res) => {
 //Listing member with id
 app.get('/users/:id', (req, res) => {
     const memberId = parseInt(req.params.id, 10);
-    db.query('SELECT * FROM members WHERE membersId = ?', [memberId], (err, results) => {
+    db.query(`SELECT m.membersId, m.memberFullName, b.bookId, b.borrowDate, b.returnDate, b.score, b.isReturned FROM members m LEFT JOIN borrows b ON m.membersId = b.membersId WHERE m.membersId = ?`, [memberId], (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.json(results);
+        const memberInformation = {
+            memberId: results[0].membersId,
+            memberFullName: results[0].memberFullName,
+            borrowedBooks: results.filter(data => data.isReturned == 1).map(data =>({
+                bookId: data.bookId,
+                borrowedDate: data.borrowDate,
+                returnDate: data.returnDate,
+                score: data.score
+            })),
+            currentBorrowingBooks: results.filter(data => data.isReturned == 0)
+            .map(data => ({
+                bookId: data.bookId,
+                borrowDate: data.borrowDate,
+            })),
+        }
+        res.json(memberInformation);
     });
 });
 
@@ -53,11 +68,27 @@ app.get('/books', (req, res) => {
 //Listing book with id
 app.get('/books/:id', (req, res) => {
     const bookId = parseInt(req.params.id, 10);
-    db.query('SELECT * FROM books WHERE bookId = ?', [bookId], (err, results) => {
+    db.query('SELECT book.bookId, book.bookName, b.score, b.isReturned FROM books book LEFT JOIN borrows b ON book.bookId = b.bookId WHERE book.bookId = ?', [bookId], (err, results) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.json(results);
+        
+        const { totalScore, count } = results.reduce((total, data) => {
+            if (data.isReturned == 1 && data.score != null) {
+                total.totalScore += data.score;
+                total.count += 1;
+            }
+            return total;
+        }, { totalScore: 0, count: 0 });
+
+        const averageScore = count > 0 ? parseFloat((totalScore / count).toFixed(2)) : 0;
+
+        const bookInformation = {
+            bookId: results[0].bookId,
+            bookName: results[0].bookName,
+            averageScore: averageScore
+        }
+        res.json(bookInformation);
     });
 });
 
@@ -71,6 +102,38 @@ app.post('/books', (req, res) => {
         res.status(201).json({ id: results.insertId, bookName });
     });
 });
+
+//Borrow a book
+app.post('/users/:membersId/borrow/:bookId', (req, res) => {
+    const membersId = parseInt(req.params.membersId, 10);
+    const bookId = parseInt(req.params.bookId, 10);
+    const borrowDate = new Date();
+    const returnDate = null;
+    db.query('INSERT INTO borrows (membersId, bookId, borrowDate, returnDate) VALUES (?, ?, ?, ?)', [membersId, bookId, borrowDate, returnDate], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ id: results.insertId, membersId, bookId, borrowDate, returnDate });
+    });
+});
+
+
+//Return a book
+app.post('/users/:membersId/return/:bookId', (req, res) => {
+    const membersId = parseInt(req.params.membersId, 10);
+    const bookId = parseInt(req.params.bookId, 10);
+    const score = req.body.score;
+    
+    const returnDate = new Date();
+    db.query('UPDATE borrows SET returnDate = ?, score = ?, isReturned = 1 WHERE membersId = ? AND bookId = ?', [returnDate, score, membersId, bookId], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ id: results.insertId, membersId, bookId, returnDate });
+    });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
